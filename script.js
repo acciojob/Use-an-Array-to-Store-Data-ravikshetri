04b2e497bbab1019732edc6da3b0382b73712fcc
ravikshetri <112544266+ravikshetri@users.noreply.github.com>
2023-04-11 16:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-
-var yourArray = ['ab', 10 , true,'abc', 30, 'str' ]
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ]
+];
